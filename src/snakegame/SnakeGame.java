a0ce7d455cc73fe302315784e3d8589262098db1
AnsHuLLranA <141855924+AnsHuLLranA@users.noreply.github.com>
2023-08-14 17:47:49 +0530

    initial commit

---
+++ src/snakegame/SnakeGame.java
@@ -0,0 +1,23 @@
+package snakegame;
+
+import javax.swing.*;
+
+public class SnakeGame extends JFrame {
+    
+    SnakeGame(){
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+        setLocationRelativeTo(null);
+        setResizable(false);
+               
+    }
+
+    
+    public static void main(String[] args) {
+        new SnakeGame().setVisible(true);
+        
+    }
+    
+}
